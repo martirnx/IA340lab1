4a6c0b07c8ab5ae9d4620967db4847d18986c05a
martirnx <143039096+martirnx@users.noreply.github.com>
2023-09-13 17:36:34 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -15,3 +15,12 @@ insert into course(c_number,c_name,room,p_email)
 values('c1','postgresql','r1','p1@jmu.edu'),
 ('c2','mongodb', 'r2','p2@jmu.edu'),
 ('c3','twitter','r1','p1@jmu.edu')
+
+--q2.4
+insert into enroll(s_email,c_number)
+values('s1@jmu.edu','c1'),
+('s2@jmu.edu','c1'),
+('s3@jmu.edu','c1'),
+('s4@jmu.edu','c2'),
+('s2@jmu.edu','c3'),
+('s3@jmu.edu','c3')
