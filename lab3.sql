cf603792565fe89ac080f8ba12695938a516811d
martirnx <143039096+martirnx@users.noreply.github.com>
2023-09-13 17:53:41 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -24,3 +24,11 @@ values('s1@jmu.edu','c1'),
 ('s4@jmu.edu','c2'),
 ('s2@jmu.edu','c3'),
 ('s3@jmu.edu','c3')
+
+--q2.5
+-- insert professor first, because
+insert into professor(p_email,p_name,office)
+values('p3@jmu.edu','p3','o3') ;
+
+insert into course(c_number,c_name,room,p_email)
+values('c4','facebook','r1','p3@jmu.edu')
